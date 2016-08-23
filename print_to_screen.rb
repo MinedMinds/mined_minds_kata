085362832d74bf95455722661a4b738475cf80d7
Jonathan Graham <jgraham@8thlight.com>
2016-08-23 11:26:07 -0400

    print numbers 1 to 100, replacing 3

---
+++ print_to_screen.rb
@@ -0,0 +1,27 @@
+number = 1
+100.times do
+	if number == 3
+		puts "Mined"
+	else 
+		puts number
+	end
+	number = number + 1
+end
+
+
+# x = 1
+# while x <= 100 do
+# 	puts x
+# 	x = x + 1
+# end 
+
+# () parentheses
+# [] square brackets
+# {} sqiggly brackets, braces, mustache
+
+#Google answers
+# print 1.step(100).to_a
+
+# (1..100).each do |whatever| 
+# 	puts whatever
+# end
